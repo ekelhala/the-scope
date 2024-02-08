354e1a9d049b8717db49fe864ba03ef896443341
Emil <emil.kelhala@gmail.com>
2024-02-08 08:55:59 +0200

    Better magnifying glass rotation

--- Assets/LensCameraRotation.cs
+++ Assets/LensCameraRotation.cs
@@ -7,15 +7,19 @@ public class LensCameraRotation : MonoBehaviour
 
     public Camera lensCamera;
     public Camera mainCamera;
+    private Transform lensTransform;
+    private Transform mainTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lensTransform = lensCamera.transform;
+        mainTransform = mainCamera.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        lensCamera.transform.rotation = mainCamera.transform.rotation;
+        Quaternion rotation = Quaternion.LookRotation(lensTransform.position - mainTransform.position, lensTransform.up);
+        lensTransform.rotation = rotation;
     }
 }
